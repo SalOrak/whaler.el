6b3cb29d230b090489cf5f366ce28606e1d3d1e2
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-03-01 10:32:05 +0100

    feat: updated commentary

--- whaler.el
+++ whaler.el
@@ -22,12 +22,18 @@
 
 ;;; Commentary:
 
-;; This package provides `whaler' as a way to move between
-;; directories blazingly fast while maintaining some sort of
-;; current working directory.
+;; This package provides `Whaler', a minimalistic and highly customizable
+;; project manager focused on tailored experiences. 
 
-;; It is a minimalistic project manager aiming to help move between
-;; directories and find files as fast as possible.
+;; It is a minimalistic project manager aiming to provide an easy API to
+;; work with repositories / directories / projects.
+
+;; `Whaler' provides functions to easily create a pool of projects as 
+;; well as interact with these projects. For example, with `Whaler' you
+;; can call `compile' in a project chosen from the minibuffer without
+;; changing the current project. But not only you can call `compile', but
+;; any function or custom function you want, interacting either in your
+;; current working project or another project selected from the minibuffer.
 
 ;;; Code:
 
